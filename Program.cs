c82cb1168771762a769b873313a7ee3fddc80ea2
juliosevillag04 <jjsevilla@uamv.edu.ni>
2026-05-20 09:36:49 -0600

    feat: Configuracion de app

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*Crear una aplicación que guarde los registros de notas de 25 estudiantes
+Debe presentar un menu:
+1. Agregar
+2. Mostrar
+3. Mostrar los 3 primeros lugares
+4. Mostrar las notas en orden descendente
+5. Salir*/
